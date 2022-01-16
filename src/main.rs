33015d7d7bbfd621b4b827b7c3d9b5ef7517c3c8
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2022-01-16 18:18:41 +0100

    Added pseudo code for setting up and running the emulator
    Signed-off-by: Emil Hammarström <emil.a.hammarstrom@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,44 @@
 use gamerboy::emulator_ram::EmuRAM;
 use gamerboy::RAM;
 
+// Gameboy EMU
 fn main() {
-    let emulator_ram = EmuRAM::<{ 8 * 1024 }>::create();
+    let emulator_ram = EmuRAM::<{ 32 * 1024 }>::create();
+    /*
+    let emulator_vram = EmuVRAM...;
+    let emulator_irq_controller = EmuIRQController...;
+    let emulator_gpu = EmuGPU...;
+    let emulator_timer = EmuTIMER...;
+
+    let emulator_bus = EmuBUS::create(ram, vram, irq_controller, timer, gpu);
+
+    let emulator_cpu = EmuCPU::create(emulator_bus);
+
+    let emulator_cartridge = EmuCartridge::from_path("pokemon.bin");
+
+    emulator_bus.map_cartridge(emulator_cartridge);
+
+    while (true) {
+        let mut cycles = 0;
+
+        // Process all IRQ requests
+        while let Some(c) = emulator_cpu.interrupt() {
+            // TODO: Figure out cycles for pushing PC, fetching vector from
+            // IRQ table and loading into PC.
+            // Some guy on the internet says this may not be an issue since
+            // timing have to be screwed up "EXTREMELY bad" for games to not
+            // work.
+            cycles += c;
+
+            emulator_bus.timer().progress(cycles);
+            emulator_bus.gpu().progress(cycles);
+        }
+
+        // Continue executing instructions
+        let cycles = emulator_cpu.step();
+
+        emulator_bus.timer().progress(cycles);
+        emulator_bus.gpu().progress(cycles);
+    }
+    */
 }
